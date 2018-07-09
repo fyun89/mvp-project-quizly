e73cc7e4e66252659303f8dad3fcc13ff7edc402
fyun89 <fyun89@gmail.com>
2018-07-09 15:02:45 -0700

    added favicon

--- mvp-quizly/src/App.js
+++ mvp-quizly/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Card from './Card.js';
 import UserInterface from "./UserInterface.js";
 import PopupList from './PopupList.js';
